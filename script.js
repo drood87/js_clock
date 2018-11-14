7dbc1c252cff75173e664e7d6b227f96f64a16ac
drood <dbernstein1987@hotmail.com>
2018-11-14 17:41:52 +1100

    added feature that all hands move according to time

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = (seconds / 60) * 360;
+    const minutes = now.getMinutes();
+    const hour = now.getHours();
+    const secondsDegrees = (seconds / 60) * 360 + 90; // + 90 to eliminte the transform 90 deg from our css in order to get the hands correct
+    const minutesDegrees = (minutes / 60) * 360 + 90;
+    const hoursDegrees = (hour / 60) * 360 + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    console.log(`${hour}:${minutes}:${seconds}`);
 }
 
-setInterval(setDate, 1000);
+setInterval(setDate, 1000); //setDate is the function
